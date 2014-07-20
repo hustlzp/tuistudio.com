16bcae7970e3405fb79312fb67c6288c481dfafb
hustlzp <hustlzp@qq.com>
2014-07-20 10:50:24 +0800

    use Flask-Bootstrap

---
+++ manage.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from tuistudio import app
+from tuistudio.models import db
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def run():
+    """启动app"""
+    app.run(debug=True, port=6666)
+
+
+@manager.command
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    manager.run()
