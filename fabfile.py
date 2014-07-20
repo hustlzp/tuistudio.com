1d3f8553d12246c2167b58458a2dfd6b306d0bed
hustlzp <hustlzp@qq.com>
2014-07-20 20:24:00 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,15 +4,15 @@ from tuistudio import config
 
 def deploy():
     env.host_string = config.HOST_STRING
-    with cd('/var/www/project'):
+    with cd('/var/www/tuistudio'):
         run('git pull')
         run('bower install --allow-root')
         run('bower update --allow-root')
         with prefix('source venv/bin/activate'):
             run('pip install -r requirements.txt')
-        run('sudo supervisorctl restart project')
+        run('supervisorctl restart tuistudio')
 
 
 def restart():
     env.host_string = config.HOST_STRING
-    run('sudo supervisorctl restart project')
+    run('supervisorctl restart tuistudio')
