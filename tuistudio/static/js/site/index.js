ac58db89cfb4281a26de6d05b8240bda2ed0b876
hustlzp <hustlzp@qq.com>
2014-07-26 21:22:49 +0800

    adjust nav condense effect

--- tuistudio/static/js/site/index.js
+++ tuistudio/static/js/site/index.js
@@ -3,28 +3,28 @@ $(function () {
 
     if (hash == '#works') {
         $.scrollTo('.works', 200, {
-            offset: -80
+            offset: -50
         });
     } else if (hash == '#services') {
         $.scrollTo('.services', 300, {
-            offset: -80
+            offset: -50
         });
     }
 
     $('#nav-works').click(function () {
         $.scrollTo('.works', 200, {
-            offset: -80
+            offset: -50
         });
     });
 
     $('#nav-services').click(function () {
         $.scrollTo('.services', 300, {
-            offset: -80
+            offset: -50
         });
     });
 
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 393) {
+        if ($(window).scrollTop() > 360) {
             $('.navbar').addClass('condensed');
         } else {
             $('.navbar').removeClass('condensed');
