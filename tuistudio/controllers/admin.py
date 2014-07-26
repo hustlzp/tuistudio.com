acc1efde7819d9a3c519d44a76eebe3ca88979fc
hustlzp <hustlzp@qq.com>
2014-07-26 22:23:57 +0800

    Add admin controller

---
+++ tuistudio/controllers/admin.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from flask import render_template, Blueprint, redirect, flash, url_for
+
+bp = Blueprint('admin', __name__)
+
+
+@bp.route('/')
+def index():
+    """管理首页"""
+    return render_template('admin/index.html')
