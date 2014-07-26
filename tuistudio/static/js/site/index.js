b0b622d2158a8105ec6d0dbd022beaf88e6a3c16
hustlzp <hustlzp@qq.com>
2014-07-26 21:15:10 +0800

    add nav condense effect

--- tuistudio/static/js/site/index.js
+++ tuistudio/static/js/site/index.js
@@ -2,15 +2,32 @@ $(function () {
     var hash = window.location.hash;
 
     if (hash == '#works') {
-        $.scrollTo('.works', 200);
+        $.scrollTo('.works', 200, {
+            offset: -80
+        });
     } else if (hash == '#services') {
-        $.scrollTo('.services', 200);
+        $.scrollTo('.services', 300, {
+            offset: -80
+        });
     }
 
     $('#nav-works').click(function () {
-        $.scrollTo('.works', 200);
+        $.scrollTo('.works', 200, {
+            offset: -80
+        });
     });
+
     $('#nav-services').click(function () {
-        $.scrollTo('.services', 300);
+        $.scrollTo('.services', 300, {
+            offset: -80
+        });
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            $('.navbar').addClass('condensed');
+        } else {
+            $('.navbar').removeClass('condensed');
+        }
     });
 });
