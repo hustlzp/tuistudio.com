64c33cc14a56b434df008d2afee12999d4474053
hustlzp <hustlzp@qq.com>
2014-07-26 20:15:24 +0800

    now can scroll

--- tuistudio/static/js/layout.js
+++ tuistudio/static/js/layout.js
@@ -2,16 +2,6 @@ $(function () {
     /* 显示flash message */
     setTimeout(showFlash, 200);
     setTimeout(hideFlash, 2000);
-
-    $('#nav-works').click(function () {
-        $.scrollTo('.works', 200);
-        return false;
-    });
-
-    $('#nav-services').click(function () {
-        $.scrollTo('.services', 300);
-        return false;
-    });
 });
 
 /**
--- tuistudio/static/js/site/index.js
+++ tuistudio/static/js/site/index.js
@@ -1,3 +1,16 @@
-$(function(){
+$(function () {
+    var hash = window.location.hash;
 
+    if (hash == '#works') {
+        $.scrollTo('.works', 200);
+    } else if (hash == '#services') {
+        $.scrollTo('.services', 200);
+    }
+
+    $('#nav-works').click(function () {
+        $.scrollTo('.works', 200);
+    });
+    $('#nav-services').click(function () {
+        $.scrollTo('.services', 300);
+    });
 });
