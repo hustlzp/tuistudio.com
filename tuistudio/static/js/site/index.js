c5f20677c67881be7b2336a9dcbee5a3dfa6f586
hustlzp <hustlzp@qq.com>
2014-07-26 21:46:40 +0800

    add contact section

--- tuistudio/static/js/site/index.js
+++ tuistudio/static/js/site/index.js
@@ -1,28 +1,29 @@
 $(function () {
     var hash = window.location.hash;
 
+    // 根据hash进行滑动
     if (hash == '#works') {
-        $.scrollTo('.works', 200, {
-            offset: -50
-        });
-    } else if (hash == '#services') {
-        $.scrollTo('.services', 300, {
-            offset: -50
-        });
+        scrollTo('.works', 200);
+    } else if (hash === '#services') {
+        scrollTo('.services', 300);
+    } else if (hash === '#contact') {
+        scrollTo('.contact', 300);
     }
 
+    // 点击导航栏滑动
     $('#nav-works').click(function () {
-        $.scrollTo('.works', 200, {
-            offset: -50
-        });
+        scrollTo('.works', 200);
     });
 
     $('#nav-services').click(function () {
-        $.scrollTo('.services', 300, {
-            offset: -50
-        });
+        scrollTo('.services', 300);
     });
 
+    $('#nav-contact').click(function () {
+        scrollTo('.contact', 300);
+    });
+
+    // 下滑到一定距离，压缩nav
     $(window).scroll(function () {
         if ($(window).scrollTop() > 360) {
             $('.navbar').addClass('condensed');
@@ -30,4 +31,15 @@ $(function () {
             $('.navbar').removeClass('condensed');
         }
     });
+
+    /**
+     * A wapper of jQuery.scrollTo
+     * @param selector
+     * @param duration
+     */
+    function scrollTo(selector, duration) {
+        $.scrollTo(selector, duration, {
+            offset: -50
+        });
+    }
 });
