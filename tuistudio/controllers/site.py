6ceb415d0efdd0a66622d6f8121664c4492d87ba
hustlzp <hustlzp@qq.com>
2014-07-27 11:27:40 +0800

    now can submit project

--- tuistudio/controllers/site.py
+++ tuistudio/controllers/site.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from flask import render_template, Blueprint, redirect, flash, url_for
 from ..forms import BusinessForm
+from ..models import db, Project
 
 bp = Blueprint('site', __name__)
 
@@ -22,6 +23,9 @@ def business():
     """Start Your Business"""
     form = BusinessForm()
     if form.validate_on_submit():
+        project = Project(**form.data)
+        db.session.add(project)
+        db.session.commit()
         flash('Got it! We will reply to you soon.')
         return redirect(url_for('.index'))
     return render_template('site/business.html', form=form)
--- tuistudio/forms/site.py
+++ tuistudio/forms/site.py
@@ -6,8 +6,9 @@ from wtforms.validators import DataRequired, Email
 
 class BusinessForm(Form):
     """提交项目意向"""
+    # type = TextField('Type', validators=[DataRequired('Project type cannot be none.')])
     bugget = TextField('Bugget', validators=[DataRequired('Bugget cannot be none.')])
     period = TextField('Period', validators=[DataRequired('Period cannot be none.')])
     desc = TextAreaField('Supplement')
-    name = TextField('Your name')
-    contact = TextField('Contact')
+    name = TextField('Your name', validators=[DataRequired('Your name cannot be none.')])
+    email = TextField('Email', validators=[DataRequired('Email cannot be none.')])
