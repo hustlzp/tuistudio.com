00ec39af21b15b1adcba0115197ed5e3feff41b0
hustlzp <hustlzp@qq.com>
2014-08-01 23:25:21 +0800

    add Google Map support

--- tuistudio/static/js/site/index.js
+++ tuistudio/static/js/site/index.js
@@ -37,6 +37,60 @@ $(function () {
         'padding-top': (bannerHeight - bannerTextHeight) * 0.45 + 'px'
     });
 
+    // Google Maps
+    var map;
+    var styleArray = [
+        {"featureType": "water", "stylers": [
+            {"color": "#46bcec"},
+            {"visibility": "on"}
+        ]},
+        {"featureType": "landscape", "stylers": [
+            {"color": "#f2f2f2"}
+        ]},
+        {"featureType": "road", "stylers": [
+            {"saturation": -100},
+            {"lightness": 45}
+        ]},
+        {"featureType": "road.highway", "stylers": [
+            {"visibility": "simplified"}
+        ]},
+        {"featureType": "road.arterial", "elementType": "labels.icon", "stylers": [
+            {"visibility": "off"}
+        ]},
+        {"featureType": "administrative", "elementType": "labels.text.fill", "stylers": [
+            {"color": "#444444"}
+        ]},
+        {"featureType": "transit", "stylers": [
+            {"visibility": "off"}
+        ]},
+        {"featureType": "poi", "stylers": [
+            {"visibility": "off"}
+        ]}
+    ];
+
+    function initialize() {
+        var location = new google.maps.LatLng(36.9685219, -85.68080429999999);
+        var marker_location = new google.maps.LatLng(36.9685219, -86.48080429999999);
+        var mapOptions = {
+            zoom: 9,
+            scrollwheel: false,
+            mapTypeControl: false,
+            center: location,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            styles: styleArray
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        var marker = new google.maps.Marker({
+            map: map,
+            position: marker_location
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
     /**
      * A wapper of jQuery.scrollTo
      * @param selector
